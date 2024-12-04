3394980769e24fc926ab9cc8acb44fdf23c08b8a
Md.Hasibul Islam <mdhasibuli478@gmail.com>
2024-12-04 10:10:14 +0600

    Add files via upload

---
+++ food-order/admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2024 All rights reserved, Food House. Developed By - <a href="#">Mst. Suraiya Akter, Md. Shezan Shikder & Md. Hasibul Islam</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
